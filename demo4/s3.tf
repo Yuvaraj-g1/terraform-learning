615da41c0d1d5117f93ced042faf966d5184ea5c
Yuvaraj-g1 <85339723+Yuvaraj-g1@users.noreply.github.com>
2021-06-18 16:08:24 +0530

    Update s3.tf

--- demo4/s3.tf
+++ demo4/s3.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
